d19dfc013e3b0db62e71048fb990598a40becb85
edewajosh <joshedewa@gmail.com>
2018-02-04 15:52:19 +0300

    Fixed the URL dispatchers

--- movies/views.py
+++ movies/views.py
@@ -4,8 +4,10 @@ from .models import Movies
 
 def index(request):
     movies = Movies.objects.order_by('-date_posted')[:6]
+    slides = Movies.objects.order_by('-date_posted')[:4]
     context = {
-        'movies' : movies
+        'movies' : movies,
+        'slides' : slides,
     }
     return render(request, 'movies/index.html', context)
 
