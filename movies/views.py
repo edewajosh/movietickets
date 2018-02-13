2e7c0928bba2a22cec0a31ab3a7b81c2da8df660
edewajosh <joshedewa@gmail.com>
2018-02-13 15:09:13 +0300

    More changes made

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Movies
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     movies = Movies.objects.order_by('-date_posted')[:6]
@@ -17,3 +20,19 @@ def details(request):
         'movies' : movies
     }
     return render(request, 'movies/details.html', context)
+
+"""
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'movies/signup.html', {'form' : form})
+"""
