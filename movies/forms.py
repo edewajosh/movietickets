a062e99a41b3e6b6454b89b7e02a687a242a8844
edewajosh <joshedewa@gmail.com>
2018-02-15 13:29:29 +0300

    Edited ticket form

--- movies/forms.py
+++ movies/forms.py
@@ -5,7 +5,13 @@ class UserRegistrationForm(forms.Form):
     email = forms.CharField(required=True, label='Email', max_length=32)
     password = forms.CharField(required=True, label='Password', max_length=32, widget=forms.PasswordInput())
 
-class TicketForm(forms.ModelForm):
+
+"""class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['username', 'phonenumber', 'numberofseats', 'seats']
+        fields = ['username', 'phonenumber', 'numberofseats', 'seats']"""
+class TicketForm(forms.Form):
+    username = forms.CharField(required=True, label="username", max_length=100)
+    phonenumber = forms.CharField(required=True, label="Phone Number", max_length=15)
+    numberofseats = forms.IntegerField(required=True, label="Number of Seats")
+    seats = forms.CharField(required=True, label="Seats", max_length=30)
