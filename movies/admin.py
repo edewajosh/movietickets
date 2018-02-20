98741ed2425cce109bf72e2751bcaf249566714f
edewajosh <joshedewa@gmail.com>
2018-02-20 12:44:57 +0300

    More work today

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Movies,Ticket
 
+class MoviesInline(admin.TabularInline):
+    model = Movies
+    extra = 3
+
 class MoviesAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_posted', 'showing_at')
+    list_filter = ["date_posted", "showing_at"]
+    search_fields = ["name"]
+    inline = [MoviesInline]
 admin.site.register(Movies, MoviesAdmin)
 
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('username','movie', 'phonenumber','numberofseats', 'seats')
+    list_filter = ["movie", "username"]
+    search_fields = ["username"]
 admin.site.register(Ticket, TicketAdmin)
