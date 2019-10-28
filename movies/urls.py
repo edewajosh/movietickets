07fece8b57dc7862b5ad6444d7f5bb36fce15cc8
edewajosh <joshedewa@gmail.com>
2019-10-28 22:43:23 +0300

    removed the black colour

--- movies/urls.py
+++ movies/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index' ),
-    path('details/', views.details, name = 'details'),
-    path('register/', views.register, name = 'register'),
-    path('ticket/', views.ticketView, name = 'ticketView'),
-    path('booked/', views.booked, name='booked'),
+    path('', views.index, name='index'),
+    path('details/', views.details, name='details'),
+    path('register/', views.register, name='register'),
+    path('ticket/', views.ticketView, name='ticketView'),
+    path('booked/<int:id>', views.booked, name='booked'),
     path('movie/<int:id>/', views.movie, name='movie'),
 
 ]
